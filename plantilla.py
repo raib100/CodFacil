aed5ed611ed87bcdf774ce26ada00f82c73589ef
raib100 <113149971+raib100@users.noreply.github.com>
2022-10-16 18:32:32 -0500

    Se crea carpeta templates y static y libreria render_template

--- plantilla.py
+++ plantilla.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 
-app = Flask(__name__)
+app = Flask(__name__) # Los html se encuentran en la carpeta templates, para cambiar:..name__,teplate_folder= 'nueva carpeta'
 
 
 @app.route('/')
