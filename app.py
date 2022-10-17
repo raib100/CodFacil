51c4378305d0c26e6d6584d4beb5af70b6e4d771
raib100 <113149971+raib100@users.noreply.github.com>
2022-10-17 11:25:18 -0500

    Ejemplo plantilla herencia

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, flash, render_template
 
 
-app = Flask(__name__)
+app= Flask (__name__)
 
 
-@app.route('/index/<name>')
-def index(name='Ricardo'):
-    MiLista = [1,2,3,4,5]
-    edad = 56
-    return render_template('index.html', name=name, edad=edad, lista=MiLista)
+@app.route('/')
+@app.route('/<name>')
+def hello(name='Ricardo'):
+    return render_template('index.html', name=name)
+    
 
-
-if __name__ == '__main__':
-    app.run(port=8000, debug=True)
+app.run(host='localhost', port=5000, debug=True)
